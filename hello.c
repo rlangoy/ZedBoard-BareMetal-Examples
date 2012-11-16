72a4d619bacf8533ce2cb129ab040d14e4a12b78
Rune <rune@RuneMint.(none)>
2012-11-16 15:02:53 +0100

    Code Cleanup

--- hello.c
+++ hello.c
@@ -1,13 +1,14 @@
-#define UART_FIFO_OFFSET	0x30 
-#define USART_BASE        0xe0001000
+ 
+#define UART0_BASE 0xe0001000
+#define UART0_TxBUFR ((unsigned int *) (UART0_BASE + 0x30))
 
-volatile unsigned int * const UART0DR = (unsigned int *) 0xe0001030;
+volatile unsigned int * const UART0TX = UART0_TxBUFR;
 	 
 void print_uart0(const char *s) 
 {
     while(*s != '\0') 
     {     /* Loop until end of string */
-	 *UART0DR = (unsigned int)(*s); /* Transmit char */
+	 *UART0TX = (unsigned int)(*s); /* Transmit char */
 	 s++; /* Next char */
     }
 }
