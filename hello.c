c26c24a8866eeeac714c0947ef58ed2e2eb913b3
Rune <rune@RuneMint.(none)>
2012-11-16 21:00:35 +0100

    Updated documentation and Names

--- hello.c
+++ hello.c
@@ -1,14 +1,25 @@
- 
-#define UART0_BASE 0xe0001000
-#define UART0_TxBUFR ((unsigned int *) (UART0_BASE + 0x30))
+/*
+    ZedBoard / Zynq-7000 
+    UART info could be found in
+        B.33 UART Controller (UART) / Page 1626 in
+           Zynq-7000
+           All Programmable SoC
+           Technical Reference Manual
+        
+        http://www.xilinx.com/support/documentation/user_guides/ug585-Zynq-7000-TRM.pdf       
+*/
 
-volatile unsigned int * const UART0TX = UART0_TxBUFR;
+ 
+#define UART1_BASE 0xe0001000
+#define UART1_TxRxFIFO0 ((unsigned int *) (UART1_BASE + 0x30))
+
+volatile unsigned int * const TxRxFIFO0 = UART1_TxRxFIFO0;
 	 
 void print_uart0(const char *s) 
 {
     while(*s != '\0') 
     {     /* Loop until end of string */
-	 *UART0TX = (unsigned int)(*s); /* Transmit char */
+	 *TxRxFIFO0 = (unsigned int)(*s); /* Transmit char */
 	 s++; /* Next char */
     }
 }
